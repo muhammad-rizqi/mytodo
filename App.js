494a43af1d5b2463b8d7f559fcaffaf2153caacc
Muhammad Rizqi <muhammad-rizqi@live.com>
2020-09-28 10:46:19 +0700

    added splash screen

--- App.js
+++ App.js
@@ -1,26 +1,46 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {Component} from 'react';
 import AppScreen from './src/screen/AppScreen';
 import DetailScreen from './src/screen/DetailScreen';
+import SplashScreen from './src/screen/SplashScreen';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Main"
-          component={AppScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Detail"
-          component={DetailScreen}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      role: true,
+    };
+    setTimeout(() => {
+      this.setState({
+        role: false,
+      });
+    }, 3000);
+  }
+
+  render() {
+    if (this.state.role) {
+      return <SplashScreen />;
+    } else {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Main"
+              component={AppScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Detail"
+              component={DetailScreen}
+              options={{headerShown: false}}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
+  }
 }
+export default App;
