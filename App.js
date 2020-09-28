6dff079a60582cc79702cfebc04cdc5dcb9f91ce
Muhammad Rizqi <muhammad-rizqi@live.com>
2020-09-28 08:58:30 +0700

    emptying project

--- App.js
+++ App.js
@@ -1,26 +1,14 @@
-import 'react-native-gesture-handler';
 import React from 'react';
-import AppScreen from './src/screen/AppScreen';
-import DetailScreen from './src/screen/DetailScreen';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {StyleSheet, Text, View} from 'react-native';
 
-const Stack = createStackNavigator();
-export default function App() {
+const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Main"
-          component={AppScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Detail"
-          component={DetailScreen}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View>
+      <Text>Hello</Text>
+    </View>
   );
-}
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
